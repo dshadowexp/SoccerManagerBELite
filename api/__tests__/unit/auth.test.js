9697517ebde0796034703bc2f61279804c80a4f0
Samuel Appiah Kubi <samkofi.appiahkubi@gmail.com>
2023-02-01 13:30:49 +0000

    save things

--- api/__tests__/unit/auth.test.js
+++ api/__tests__/unit/auth.test.js
@@ -1,9 +1,16 @@
 import { authenticationMiddleware } from './../../../middleware/auth.js';
+import { User } from './../../users/model.js';
+import { Types } from "mongoose";
 
 describe('Auth Middleware', () => {
+    let token;
+
+    beforeEach(() => {
+        token = User({ _id: Types.ObjectId.generate() }).generateAuthToken()
+    })
 
     it('should ', () => {
-        const req = {};
+        const req = {'x-auth-token': token};
         const res = {};
         const next = jest.fn();
         authenticationMiddleware(req, res, next);
